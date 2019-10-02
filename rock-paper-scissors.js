cafb90069e8fc1c426fd847f031e82a06ed69bb4
kattbaum <katt.baum@gmail.com>
2019-10-02 15:44:28 -0400

    Added readline for user input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -15,9 +15,14 @@ Our code will break the game into four parts:
     Start the program and display the results. */
 
 
-//let userInput = 'bomb'
+let userInput = 'bomb'
 
-import { question } from 'readline';
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const getUserChoice = userInput => {
   userChoice = userInput.toLowerCase()
@@ -40,24 +45,24 @@ const getComputerChoice = () => {
 
 const determineWinner = (userChoice,compChoice) => {
   //Tie!
-  if(userChoice === compChoice) return 'the game is tied!'
+  if(userChoice === compChoice) return 'the game is tied!\n\n'
   
   //Determine winner!
   if(userChoice === 'rock') {
-    if(compChoice === 'paper') return 'the computer has won!'
-    if(compChoice === 'scissors') return 'the human has won!'
+    if(compChoice === 'paper') return 'the computer has won!\n\n'
+    if(compChoice === 'scissors') return 'the human has won!\n\n'
   }
   if(userChoice === 'paper') {
-    if(compChoice === 'scissors') return 'the computer has won!'
-    if(compChoice === 'rock') return 'the human has won!'
+    if(compChoice === 'scissors') return 'the computer has won!\n\n'
+    if(compChoice === 'rock') return 'the human has won!\n\n'
   }
   if(userChoice === 'scissors') {
-    if(compChoice === 'rock') return 'the computer has won!'
-    if(compChoice === 'paper') return 'the human has won!'
+    if(compChoice === 'rock') return 'the computer has won!\n\n'
+    if(compChoice === 'paper') return 'the human has won!\n\n'
   }
 }
 
-const playGame = (userInput) => {
+const playGame = () => {
   const compChoice = getComputerChoice()
   let userChoice = getUserChoice(userInput)
   //Bomb cheat!
@@ -67,16 +72,14 @@ const playGame = (userInput) => {
     if(compChoice === 'scissors') userChoice = 'rock'
   }
   //Announce game!
-  console.log(`human vs computer!\n${userChoice} vs ${compChoice}!`)
+  console.log(`\nhuman vs computer! \n${userChoice} vs ${compChoice}!`)
   //Announce winner!
   console.log(determineWinner(userChoice,compChoice))
 }
 
 
-// question('rock, paper, or scissors?', playGame(userInput)) {
-//     close()
-// }
-
-question('What is your favorite food? ', (answer) => {
-    console.log(`Oh, so your favorite food is ${answer}`);
-  });
+rl.question('\nrock, paper, or scissors? ', (answer) => {
+  userInput = answer
+  playGame()
+  rl.close()
+})
