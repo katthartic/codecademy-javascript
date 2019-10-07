5fb80b7caebf55dfc811c57c8e1de938e6be270c
kattbaum <katt.baum@gmail.com>
2019-10-07 16:40:25 -0400

    Added generateTarget, compareGuesses, updateScore, and advanceRound

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,29 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random() * 10)
+
+// function generateTarget() {
+//     let randNum = Math.floor(Math.random() * 10)
+//     return randNum
+// }
+
+function compareGuesses(userGuess, compGuess, target = generateTarget()) {
+    let userDistance = Math.abs(target - userGuess)
+    let compDistance = Math.abs(target - compGuess)
+
+    if (userDistance <= compDistance) return true
+    else return false
+}
+
+function updateScore(winner) {
+    if (winner === 'human') humanScore++
+    else computerScore++
+}
+
+function advanceRound() {
+    currentRoundNumber++
+}
+
+
+
